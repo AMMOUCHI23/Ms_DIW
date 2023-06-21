587d8e24df1bd6c2d1293b42af57ee9d6d0e3adb
Ammouchi abdallah <bily-f@hotmail.fr>
2023-06-21 23:37:38 +0200

    js formulaire projet finalisé

--- Projet_fil_rouge/assets/javascript/script.js
+++ Projet_fil_rouge/assets/javascript/script.js
@@ -1,30 +1,61 @@
 console.log("Je suis connecté...!  :)");
 
 document.forms["contact"].addEventListener("submit", function (e) {
-	var inputs = this;
-	var nom_v=/^[a-zA-Z\s]+$/;
-	var email_v=/^\w+@[a-zA-Z_-]+?\.[a-zA-Z]{2,3}$/;
+    const inputs = this;
+	const nom_v=/^[a-zA-Z\s]+$/;
+	const email_v=/^[a-zA-Z0-9.-_]+@{1}[a-zA-Z_-]+[.]{1}[a-zA-Z]{2,3}$/;
+	const telephone_v=/^0[1-9]{9}$/;
+	const text_v=/^[a-zA-Z\s]{1,300}$/;
 	
-	var telephone_v=/^(0[1-68])(?:[ _.-]?(\d{2})){4}$/;
-	e.preventDefault();
 	if (nom_v.test(this["nom"].value.trim() )== false) {
-		document.getElementById("nom_inv").innerHTML = "Nom invalide";
+		e.preventDefault();
+		document.getElementById("nom_inv").innerHTML = "Nom invalide"
+	}else {
+			document.getElementById("nom_inv").innerHTML = "";
 	}
-	else if (nom_v.test(this["prenom"].value.trim() )== false) {
+	
+
+	
+    if (nom_v.test(this["prenom"].value.trim() )== false) {
+		e.preventDefault();
 		document.getElementById("prenom_inv").innerHTML = "Prenom invalide";
 	}
-	else if (email_v.test(this["email"].value.trim() )== false) {
+	else {
+		document.getElementById("prenom_inv").innerHTML = "";
+	}
+
+   if (email_v.test(this["email"].value.trim().split(" ").join("") )== false) {
+	e.preventDefault();
 		document.getElementById("email_inv").innerHTML = "Email invalide";
 	}
-	else if (telephone_v.test(this["telephone"].value.trim() )== false) {
+	else {
+		document.getElementById("email_inv").innerHTML = "";
+	}
+	 if (telephone_v.test(this["telephone"].value.trim().split(" ").join("") )== false) {
+		e.preventDefault();
 		document.getElementById("telephone_inv").innerHTML = "Numéro de téléphone invalide";
+		e.preventDefault();
+	}
+	else{
+		
+		document.getElementById("telephone_inv").innerHTML = "";
+		e.preventDefault();
+
+	}
+	if (text_v.test(this["text"].value.trim() )== false) {
+		e.preventDefault();
+		document.getElementById("text_inv").innerHTML = "Le champs est requis ";
+	}
+	else {
+		document.getElementById("text_inv").innerHTML = "";
 	}
 
-     else {
-	  alert("Formulaire envoyé")
-         }
+   if(document.getElementById("nom_inv").innerHTML == "" && document.getElementById("prenom_inv").innerHTML == "" && document.getElementById("email_inv").innerHTML == "" 
+   && document.getElementById("telephone_inv").innerHTML == "" && document.getElementById("text_inv").innerHTML == ""){
+	alert("formulaire envoyé avec succé")
+   }
+    
 });
-
 /*	for (let i = 0; i < inputs.length; i++) {
 		 if(!inputs[i].value){
 			ereur="veuillez renseigner tout les champs";
