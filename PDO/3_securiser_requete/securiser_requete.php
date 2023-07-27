699d8f6e752634e2c395c85a173a3e77c735bc13
Ammouchi Abdallah <bily-f@hotmail.fr>
2023-07-27 16:55:05 +0200

    jointure sql

--- PDO/3_securiser_requete/securiser_requete.php
+++ PDO/3_securiser_requete/securiser_requete.php
@@ -8,8 +8,22 @@
 </head>
 <body>
     <?php
-  
-     
+    /*
+    Sécuriser les requêtes PHP est essentiel pour protéger votre application contre les attaques 
+    potentielles telles que les injections SQL et d'autres...
+    Voici quelques pratiques recommandées pour sécuriser vos requêtes PHP :
+
+   1- Utilisez des requêtes préparées (Prepared Statements) :
+    Les requêtes préparées permettent de séparer les données des instructions SQL,
+    réduisant ainsi le risque d'injection SQL. 
+    
+    2-Validez et échappez les données utilisateur :
+    Avant d'utiliser les données provenant des utilisateurs, validez-les et échappez-les 
+    correctement pour empêcher les attaques XSS et autres problèmes de sécurité. 
+    Utilisez des fonctions comme filter_input(), htmlspecialchars() ou des bibliothèques 
+    de validation de données.
+
+    */
     $DB_DSN= "mysql:host=localhost;dbname=test";
     $DB_USER= "root";
     $DB_PASS= "Maformation";
@@ -20,6 +34,25 @@
     {
      // query(): permet de renvoyer les données repondant à une ou plusieurs conditions
       $pdo = new PDO ($DB_DSN,$DB_USER,$DB_PASS,$option);
+
+      //préparer la requete
+      $requete=$pdo->prepare("SELECT *FROM utilisateur WHERE nom=?");
+      /* bindValue(): permet de lier des valeurs à des parametres et permet d'exicuter 
+      une requete plusieurs fois juste en changeant les parametres, elle évalue la requete 
+      au moment de l'appeler
+      bindParam(): fait le meme travaille que bindValue(), sauf elle évalue la requete
+      au moment de son exicution par (execute()).
+      !!! au lieu de nom=? on peut ecrire :nom et aprés bindValue(nom,"AMMOUCHI")
+      */
+      $requete->bindValue(1,$nom);
+
+      // Éxicuter la requete
+      $requete->execute();
+
+      //afficher la requete
+      echo"<pre>";
+      print_r($requete->fetch(PDO::FETCH_ASSOC));
+      echo"<pre>";
       echo "connexion à la base de données réussie";
     }
      catch (PDOException $e)
--- phpMyAdmin/jointure_union.php
+++ phpMyAdmin/jointure_union.php
@@ -12,11 +12,18 @@
        une interne
        - INNER JOIN : retourne des résultats sauf s'il y a une correspondance entre les deux tables.
        et tois dites externes: retournent des résultats meme si il n y a pas de correspondance entre les deux tables
-       -FULL OUTER JOIN
-       -LEFT JOIN
-       -RIGHT JOIN
+       -FULL OUTER JOIN : elle n'est pas supportée par mysql on utilise union à la place
+       -LEFT JOIN : elle permet de récupérer tout le contenu de la table de gauche, et seulement 
+       les éléments de la table de droite qui ont une correspondance avec la table de gauche
+       -RIGHT JOIN : elle fait le contraire de la jointure LEFT JOINT
+       -UNION : opérateur est utilisé pour combiner le jeu de résultats de deux SELECT ou plus.
+                 Chaque SELECT instruction à l'intérieur UNION doit avoir le même nombre de colonnes
+                 Les colonnes doivent également avoir des types de données similaires
+                 Les colonnes de chaque SELECTinstruction doivent également être dans le même ordre
+                 Il envois que la premiere récurence de meme valeur (s'il ya plusieurs nom identique -> il renvois juste le premier nom)
+        -UNION ALL : il renvoit toutes le récurence de meme valeure       
+                 */ 
 
-    */
     
     $serveur="localhost";
     $login="root";
@@ -39,6 +46,12 @@
     echo "<pre>";
     print_r($resultat);
     echo "<pre>";
+
+    /*  exemple avec UNION
+        SELECT prenom FROM inscrit
+        UNION ALLL
+        SELECT commentaire FROM commentaire
+    */
 
     }
     catch (PDOException $e){
